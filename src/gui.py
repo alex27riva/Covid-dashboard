850083cbede75695984bfbaa7ddba5f1de8cbb90
Alessandro Riva <alex27riva@gmail.com>
2020-09-17 18:41:46 +0200

    Progress

--- src/gui.py
+++ src/gui.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-
 from tkinter import *
 from tkinter.ttk import *
-import tkinter.messagebox
 
 root = Tk()
 
 style = Style()
 
-style.configure('TButton', font = 
-               ('calibri', 10, 'bold'), 
-                foreground = 'black')
+style.configure('TButton', font=
+('calibri', 10, 'bold'),
+                foreground='black')
 
 # set title
 
@@ -21,33 +19,36 @@ root.geometry('300x300')
 # don't resize window
 root.resizable(False, False)
 
+
 def download_data():
-    import getalldata
+    pass
+
 
 def open_rstudio():
     pass
 
+
 def source_files():
     pass
+
 
 def open_pdf():
     pass
 
 
-button_download = Button(root, text ="Download data", command = download_data)
-button_download.pack(side=TOP, expand = True, fill = 'both')
+button_download = Button(root, text="Download data", command=download_data)
+button_download.pack(side=TOP, expand=True, fill='both')
 
-button_open_rstudio = Button(root, text ="Open RStudio", command = open_rstudio)
-button_open_rstudio.pack(side=TOP, expand = True, fill = 'both')
+button_open_rstudio = Button(root, text="Open RStudio", command=open_rstudio)
+button_open_rstudio.pack(side=TOP, expand=True, fill='both')
 
-button_source_files = Button(root, text ="Source files", command = source_files)
-button_source_files.pack(side=TOP, expand = True, fill = 'both')
+button_source_files = Button(root, text="Source files", command=source_files)
+button_source_files.pack(side=TOP, expand=True, fill='both')
 
-button_open_pdf = Button(root, text ="Open dashboard (PDF)", command = open_pdf)
-button_open_pdf.pack(side=TOP, expand = True, fill = 'both')
+button_open_pdf = Button(root, text="Open dashboard (PDF)", command=open_pdf)
+button_open_pdf.pack(side=TOP, expand=True, fill='both')
 
-button_open_dashboard = Button(root, text ="Open dashboard panel", command = open_pdf)
-button_open_dashboard.pack(side=TOP, expand = True, fill = 'both')
-
+button_open_dashboard = Button(root, text="Open dashboard panel", command=open_pdf)
+button_open_dashboard.pack(side=TOP, expand=True, fill='both')
 
 root.mainloop()
