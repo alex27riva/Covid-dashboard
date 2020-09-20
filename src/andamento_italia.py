1455c35874fd76bceb936b9a98316539234301b3
Alessandro Riva <alex27riva@gmail.com>
2020-09-20 10:24:02 +0200

    [update] andamento italia

--- src/andamento_italia.py
+++ src/andamento_italia.py
@@ -1,28 +1,35 @@
 #!/usr/bin/python3
 import pandas as pd
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 url_andamento_nazionale = 'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale.csv'
 
 df = pd.read_csv(url_andamento_nazionale, index_col=[], usecols=['data', 'totale_positivi', 'nuovi_positivi'])
 
-graph_data = [
-    go.Bar(
-        x=df['data'],
-        y=df['nuovi_positivi']
-    ),
-    go.Scatter(
-        x=df['data'],
-        y=df['totale_positivi']
-    )
+fig = make_subplots(specs=[[{"secondary_y": True}]])
 
-]
+fig.add_trace(
+    go.Bar(x=df['data'], y=df['nuovi_positivi'],
+           name='nuovi positivi'),
+    secondary_y=True)
 
-layout = go.Layout(
-    barmode='stack',
-    title='Stacked Bar with Pandas'
+fig.add_trace(
+    go.Scatter(x=df['data'],
+               y=df['totale_positivi'],
+               name='totale positivi'),
+    secondary_y=False,
 )
-
-fig = go.Figure(data=graph_data, layout=layout)
+# Add title
+fig.update_layout(
+    title_text="Andamento dei contagi Italia",
+    barmode='stack',
+    autosize=True
+)
+# set x axis name
+fig.update_xaxes(title_text="Giorni")
+# set y axis title
+fig.update_yaxes(title_text="Contagi totali", secondary_y=False)
+fig.update_yaxes(title_text="Contagi giornalieri", secondary_y=True)
 
 fig.show()
