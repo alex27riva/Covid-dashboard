5b484a619a378521c662e8c9bcfc99e2018d0f19
Alessandro Riva <alex27riva@gmail.com>
2020-10-09 18:42:52 +0200

    Updated dataset

--- src/dashboard/Util.py
+++ src/dashboard/Util.py
@@ -1,7 +1,23 @@
-class Util:
-    """This class is usedto derive more data"""
-    def __init__(self):
-        pass
+import pandas as pd
 
-    def calc_normalized_tests(self, test_values):
-        pass
+
+class Util:
+    """This class is used to derive more data from .csv file"""
+
+    def __init__(self, file):
+        self.df = pd.read_csv(file)
+
+    def print_colunm_names(self):
+        print(self.df.columns)
+
+    def column_list(self):
+        return list(self.df.columns)
+
+    def calc_delta(self, col, new_col):
+        self.df[new_col] = self.df.col.diff().shift(-1)
+
+
+test_url = 'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita' \
+           '-andamento-nazionale.csv '
+u = Util(test_url)
+print(u.column_list())
