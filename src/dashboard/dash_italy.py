03513fe223bfc3862bf84c7445a5a6281be10d96
Alessandro Riva <alex27riva@gmail.com>
2020-10-15 22:22:10 +0200

    Clean dash

--- src/dashboard/dash_italy.py
+++ src/dashboard/dash_italy.py
@@ -11,93 +11,15 @@ url = 'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-
 # read csv for url
 df = pandas.read_csv(url)
 
-external_stylesheets = ['https://codepen.io/amyoshino/pen/jzXypZ.css']
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-# app = dash.Dash()
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-app.title = 'Dashboard Italia'
+app_title = 'Dashboard Italia'
 
 # data calculation
 df['terapia_intensiva_avg'] = df['terapia_intensiva'].rolling(7).mean()
-
-
-# deaths curve
-def deaths_curve():
-    df['deceduti_giorn'] = df.deceduti.diff().shift(-1)
-    df['moving_avg'] = df['deceduti_giorn'].rolling(7).mean()
-    fig = go.Figure(
-        go.Bar(x=df['data'], y=df['deceduti_giorn'],
-               name='Deaths',
-               marker_color='orange')
-
-    )
-    fig.add_trace(
-        go.Scatter(x=df['data'], y=df['moving_avg'],
-                   name='7 day avg'
-
-                   ))
-
-    fig.update_layout(
-        title_text='Daily deaths'
-    )
-    fig.update_xaxes(title_text="Days")
-    return fig
-
-
-# chart 13
-def home_isolation():
-    fig = go.Figure(
-        go.Bar(x=df['data'], y=df['isolamento_domiciliare'],
-               name='Home Isolation',
-               marker_color='SlateGray')
-
-    )
-
-    fig.update_layout(
-        title_text='Home isolation'
-    )
-    fig.update_xaxes(title_text="Days")
-    return fig
-
-
-def normalized_new_cases():
-    MIN_DELTA_TAMP = 964  # =MIN(Q$7:Q$119)    Q = delta_tamp
-    REF_TAMP = 48000  # reference value
-
-    # column names
-    x_name = 'data'
-    y_moving_7gg = 'delta_cases_average'
-    df_loc = df.copy()
-    df_loc = df_loc[77:]
-    df_loc['delta_tamponi'] = df_loc.tamponi.diff().fillna(df_loc.tamponi)
-    df_loc['tamp_norm'] = MIN_DELTA_TAMP / df_loc['delta_tamponi'] * df_loc['nuovi_positivi']
-    df_loc['nuovi_casi_norm'] = df_loc['nuovi_positivi'] * REF_TAMP / df_loc['delta_tamponi']
-
-    # rolling average 7gg
-    df_loc[y_moving_7gg] = df_loc['nuovi_casi_norm'].rolling(7).mean()
-
-    fig = go.Figure(
-        go.Bar(x=df_loc[x_name], y=df_loc['nuovi_casi_norm'].astype(int),  # convert to int
-               name='New cases')
-    )
-
-    fig.add_trace(
-        go.Scatter(x=df_loc[x_name],
-                   y=df_loc[y_moving_7gg],
-                   name='7 day average')
-    )
-
-    fig.update_layout(
-        title_text='Normalized new daily cases in Italy (+ 7 day avg)'
-    )
-    fig.update_xaxes(title_text="Days")
-    fig.update_yaxes(title_text="Normalized daily cases")
-    return fig
-
-
-# Bootstrap CSS
-# app.css.append_css({'external_url': 'https://codepen.io/amyoshino/pen/jzXypZ.css'})
+df['nuovi_decessi'] = df.deceduti.diff().fillna(df.deceduti)
 
 app.layout = html.Div(  # main div
     html.Div([
@@ -106,28 +28,30 @@ app.layout = html.Div(  # main div
                 src='https://www.uninsubria.it/sites/all/themes/uninsubria/logo.png',
                 className='three columns',
                 style={
-                    'height': '9%',
-                    'width': '9%',
+                    'height': '8%',
+                    'width': '8%',
                     'float': 'right',
                     'position': 'relative',
                 },
             ),
-            html.H1(children='Dashboard Italy',
+            html.H1(children='Dashboard Italia',
                     className='nine columns'),
 
-            html.Div(children='Covid19 dashboard for Italy',
+            html.Div(children='Situazione Covid-19 in Italia',
                      className='nine columns')
 
         ], className='row'),
 
-        html.Div([  # div for charts
+        html.Div([  # first chart row
             html.Div([
                 dcc.Graph(
-                    id='cumulative-icu',
+                    id='Terapia-intensiva',
                     figure={
                         'data': [
-                            {'x': df['data'], 'y': df['terapia_intensiva'], 'type': 'bar', 'name': 'Terapia Intensiva'},
+                            {'x': df['data'], 'y': df['terapia_intensiva'], 'type': 'bar', 'name': 'Terapia Intensiva',
+                             'marker': dict(color='DodgerBlue')},
                             {'x': df['data'], 'y': df['terapia_intensiva_avg'], 'type': 'scatter',
+                             'line': dict(color='blue'),
                              'name': '7 day moving avg'}
                         ],
                         'layout': {
@@ -137,12 +61,61 @@ app.layout = html.Div(  # main div
                     config={
                         'displaylogo': False,
                         'displayModeBar': False
+                    }
+                )
+            ], className='six columns'),
+            html.Div([
+                dcc.Graph(
+                    id='Casi-totali',
+                    figure={
+                        'data': [
+                            {'x': df['data'], 'y': df['totale_casi'], 'type': 'bar', 'name': 'Casi totali'},
+                        ],
+                        'layout': {
+                            'title': 'Casi totali'
+                        }
                     },
-                    style={
-                        'margin-top': 20
+                    config={
+                        'displaylogo': False,
+                        'displayModeBar': False
                     }
                 )
             ], className='six columns')
+        ], className='row'),
+
+        html.Div([  # second chart row
+            html.Div([
+                dcc.Graph(
+                    id='nuovi-casi-vs-morti',
+                    figure={
+                        'data': [
+                            {'x': df['data'], 'y': df['nuovi_decessi'], 'type': 'bar', 'name': 'Morti giornalieri',
+                             'marker': dict(color='DimGrey')},
+                            {'x': df['data'], 'y': df['nuovi_positivi'], 'type': 'scatter', 'xaxis': 'x2',
+                             'yaxis': 'y2',
+                             'line': dict(color='blue'),
+                             'name': 'Nuovi casi'}
+                        ],
+                        'layout': {
+                            'yaxis': {
+                                'title': 'Nuovi casi vs morti giornalieri',
+                                'secondary_y': False
+                            },
+                            'yaxis2': {
+                                'title': 'Nuovi casi vs morti giornalieri',
+                                'secondary_y': True
+                            }
+
+                        }
+                    },
+                    config={
+                        'displaylogo': False,
+                        'displayModeBar': False
+                    }
+                )
+
+            ], className='twelve columns')
+
         ], className='row')
     ], className='ten columns offset-by-one')
 )
