85152a9f1e783d590b9f877b4136c419c1f4f5fb
Alessandro Riva <alex27riva@gmail.com>
2020-10-19 11:14:53 +0200

    Cleanup

--- src/grafici/lombardia/normalized_daily_cases.py
+++ src/grafici/lombardia/normalized_daily_cases.py
@@ -1,26 +1,30 @@
 #!/usr/bin/python3
 import pandas as pd
 import plotly.graph_objects as go
+
 """
 Title: Normalizzed new daily cases in Regione Lombardia
 Description: This chart shows a bar chart of normalized new cases in Lombardia
 """
-# todo: Remove old dataset
-dataset = '../../../dataset/lombardia.csv'
+url = 'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni.csv'
+REF_TAMP = 9000  # reference value
+
 # chart title
 chart_title = "Normalized new daily cases in Regione Lombardia"
 
-# column names
-x_name = 'data'
-y_name = 'new_cases_normalized'
+df = pd.read_csv(url)
+df = df.loc[df['denominazione_regione'] == 'Lombardia']
 
-df = pd.read_csv(dataset, index_col=[], usecols=[x_name, y_name])
 # trim lines (missing data)
 df = df[136:]
 
+# calculate
+df['incr_tamponi'] = df.tamponi.diff().fillna(df.tamponi)
+df['nuovi_casi_norm'] = df['nuovi_positivi'] * REF_TAMP / df['incr_tamponi']
+
 fig = go.Figure(
-    go.Bar(x=df[x_name], y=df[y_name],
-           name='New cases')
+    go.Bar(x=df['data'], y=df['nuovi_casi_norm'],
+           name='New cases normalized')
 )
 # Add title
 fig.update_layout(
